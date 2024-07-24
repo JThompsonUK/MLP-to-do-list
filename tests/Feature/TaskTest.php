09e86ee01fbae4b228357c20fb796fef12379408
Jamie Thompson <jamiethompson.uk@gmail.com>
2024-07-24 22:52:29 +0100

    added feature tests

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $newTask;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->newTask = Task::factory()->create();
+    }
+
+    /**
+     * Test creating a new task.
+     *
+     * @test
+     */
+    public function it_can_create_a_task()
+    {
+        $response = $this->post('/tasks/store', ['name' => 'New Task']);
+
+        $response->assertRedirect(route('tasks.index'));
+        $response->assertSessionHas('success', 'Task created successfully.');
+        $this->assertDatabaseHas('tasks', ['name' => 'New Task']);
+    }
+
+    /**
+     * Test marking a task as completed.
+     *
+     * @test
+     */
+    public function it_marks_task_as_completed_when_ticked()
+    {
+        $response = $this->get(route('tasks.tick', $this->newTask->id));
+
+        $response->assertRedirect(route('tasks.index'));
+        $response->assertSessionHas('success', 'Task marked as completed.');
+
+        $this->assertDatabaseHas('tasks', [
+            'id' => $this->newTask->id,
+            'completed' => Carbon::now()
+        ]);
+    }
+
+    /**
+     * Test marking a task as not completed.
+     *
+     * @test
+     */
+    public function it_marks_task_as_not_completed_when_unticked()
+    {
+        $response = $this->get(route('tasks.untick', $this->newTask->id));
+
+        $response->assertRedirect(route('tasks.index'));
+        $response->assertSessionHas('success', 'Task marked as not completed.');
+
+        $this->assertDatabaseHas('tasks', [
+            'id' => $this->newTask->id,
+            'completed' => null
+        ]);
+    }
+
+    /**
+     * Test deleting a task.
+     *
+     * @test
+     */
+    public function it_can_delete_a_task()
+    {
+
+        $response = $this->delete("/tasks/{$this->newTask->id}/delete");
+
+        $response->assertRedirect(route('tasks.index'));
+        $response->assertSessionHas('success', 'Task deleted successfully.');
+
+        $this->assertDatabaseMissing('tasks', ['id' => $task->id]);
+    }
+}
